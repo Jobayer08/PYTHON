b1f6d5535fd791de1291d6b847a2c476fe6dbb35
Jobayer <mdabduljobayer570@gmail.com>
2024-12-29 19:50:20 +0600

    String

---
+++ lecture_2_string.py
@@ -0,0 +1,24 @@
+#string operation
+
+str1='my'
+str2='name'
+final_str=str1+str2
+print(final_str)
+
+print(len(final_str))
+
+print(final_str[3])
+
+print(final_str[1:3])
+print(final_str[1:])
+print(final_str[:5])
+print(final_str[-5:-1])
+
+#string function
+
+str="i am jobayer"
+print(str.endswith('er'))
+print(str.capitalize())
+print(str.replace('a','e'))
+print(str.find('e'))
+print(str.count('a'))
