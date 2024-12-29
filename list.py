0204dac8bfa94234fede6ee3319d805f54a141d6
Jobayer <mdabduljobayer570@gmail.com>
2024-12-30 01:48:32 +0600

    list

---
+++ list.py
@@ -0,0 +1,19 @@
+student=["jobayer",89,98.8]
+print(student[0])
+student[1] =True
+print(student)
+
+#list methods
+
+list=[2,1,4]
+list.append(6)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+list.reverse()
+list.insert(3,7)
+print(list)
+list.remove(2)
+list.pop(1)
+print(list)
---
+++ practice_10_palin.py
@@ -0,0 +1,8 @@
+list=[1,2,3,2,1]
+l2=list.copy()
+list.reverse()
+if(list==l2):
+    print("yes")
+else:
+    print("no") 
+     
