1dbb700c310c871b321c56dba35f2de8965c142b
Jobayer <mdabduljobayer570@gmail.com>
2024-12-29 16:50:32 +0600

    Create First_program.py

---
+++ First_program.py
@@ -0,0 +1,101 @@
+print("Hello world!","My name is Jobayer")
+
+name="Jobayer"
+age=23
+price=25.99
+
+print("my name is :",name)
+print("my age is :",age)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+name1="jobayer"
+name2='Jobayer'
+name3='''jobayer'''
+
+print(name1,name2,name3)
+
+old=False
+a=None
+print(type(a),type(old))
+
+# hey
+"""
+hey
+"""
+#Arithmetic operators
+
+a=5
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+#Relational operators
+
+a=50
+b=20
+
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a>b)
+print(a<=b)
+print(a<b)
+
+#Assignment operators
+
+num=10
+num+=10
+print("num:", num)
+num=10
+num-=10
+print("num:", num)
+num=10
+num*=10
+print("num:", num)
+num=10
+num/=10
+print("num:", num)
+num=10
+num%=10
+print("num:", num)
+num=10
+num**=10
+print("num:", num)
+
+#Logical operators
+
+a=50
+b=30
+print(not False)
+print(not (a>b))
+
+va1=True
+va2=False
+print("and operator:",va1 and va2)
+print("or operator:",va1 or va2)
+
+#type conversions
+
+a=2
+b=2.33
+sum=a+b
+print(sum)
+#type casting
+a="2"
+b=2.33
+sum=int(a)+b
+print(sum)
+
+name=input("enter your name: ")
+print("welcome",name)
+
+
+
